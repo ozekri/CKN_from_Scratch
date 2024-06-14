45529e59dbaf6be754ffc4f88d4829fb9e12166e
Ambroise Odonnat <ambroiseodonnattechnologie@gmail.com>
2024-06-14 17:14:44 +0200

    Add documentation

--- gpu/CUDA/correlate2d_gpu.cu
+++ gpu/CUDA/correlate2d_gpu.cu
@@ -1,8 +1,31 @@
+/*!
+Cuda implementation of scipy.signal.correlate2d.html.
+See <https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.correlate2d.html> for
+the origial implementation.
+*/
+
+
 extern "C" {
-    __global__ void correlate2d_gpu_kernel(float* result, float* image, float* kernel, int image_width, int image_height, int kernel_width, int kernel_height) {
+    __global__ void correlate2d_gpu_kernel(
+        float* result,
+        float* image,
+        float* kernel,
+        int image_width,
+        int image_height,
+        int kernel_width,
+        int kernel_height) {
+        /*!
+        results: Contains the discrete linear cross-correlation of image with kernel.
+        image: First input.
+        kernel: Second input.
+        image_width: Width of first input.
+        image_height: Height of first input.
+        kernel_width: Width of second input.
+        kernel_height: Height of second input.
+        */
         int i = blockIdx.x * blockDim.x + threadIdx.x;
         int j = blockIdx.y * blockDim.y + threadIdx.y;
-        
+
         if (i < image_width - kernel_width + 1 && j < image_height - kernel_height + 1) {
             float sum = 0.0f;
             for (int ki = 0; ki < kernel_width; ki++) {
@@ -13,8 +36,24 @@ extern "C" {
             result[i * (image_height - kernel_height + 1) + j] = sum;
         }
     }
-    
-    void correlate2d_gpu(float* result, float* image, float* kernel, int image_width, int image_height, int kernel_width, int kernel_height) {
+
+    void correlate2d_gpu(
+        float* result,
+        float* image,
+        float* kernel,
+        int image_width,
+        int image_height,
+        int kernel_width,
+        int kernel_height) {
+        /*!
+        results: Contains the discrete linear cross-correlation of image with kernel.
+        image: First input.
+        kernel: Second input.
+        image_width: Width of first input.
+        image_height: Height of first input.
+        kernel_width: Width of second input.
+        kernel_height: Height of second input.
+        */
         float* d_result;
         float* d_image;
         float* d_kernel;
